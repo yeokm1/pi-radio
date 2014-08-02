fe475ef655a00a774044b92b8b34a7c94b198fc6
Yeo Kheng Meng <yeokm1@yahoo.com.sg>
2014-08-02 18:47:47 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #ping until reach WAN then start pi radio
-for i in {1..50}; do ping -c1 www.google.com.sg &> /dev/null && break;
+while true; do ping -c1 www.google.com.sg &> /dev/null && break; done
 cd /root/pi-radio
 /usr/bin/python2 radio.py &
