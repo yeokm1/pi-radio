bfa500e46af28dd91c9682bebf3272cdc3fc1993
Yeo Kheng Meng <yeokm1@gmail.com>
2014-08-06 19:24:44 +0800

    4th station

--- radio.py
+++ radio.py
@@ -23,7 +23,7 @@ regex = re.compile(findVolumeRegex)
 
 stationsList = []
 lcd = Adafruit_CharLCDPlate()
-stationIndex = 2  #I want to play my third station be default
+stationIndex = 3  #I want to play my fourth station as default
 numStations = None
 
 
